6136667b0036f9129ebe7afe82ecf94d04e72bf8
unknown <srijandas18.sd@gmail.com>
2022-12-22 11:58:28 +0530

    updated skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 type Props = {
-  directionLeft: Boolean;
+  directionLeft: boolean;
 };
 
 function Skill({ directionLeft }: Props) {
