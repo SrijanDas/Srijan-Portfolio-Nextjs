230552c50c36512225af35365a134e43ed884f2a
unknown <srijandas18.sd@gmail.com>
2022-12-29 00:49:50 +0530

    removed axios proxy

--- config/axios.ts
+++ config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    proxy: false,
+    // proxy: false,
     baseURL: process.env.NEXT_PUBLIC_BASE_URL
     // baseURL: window.location.hostname
   });
